15e0ff92ef165bae390b3244444a35eb9499b551
Alexgoon <alex.russkovdx@gmail.com>
2017-02-27 11:53:30 +0300

    fx

--- CS/Example/MainWindow.xaml.cs
+++ CS/Example/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Example {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            int t=12345dsfasd;
+            int t=12345;
         }
     }
 }
