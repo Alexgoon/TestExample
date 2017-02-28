a612693cef1b23ddf21198c70832367569bec11b
Alexgoon <alex.russkovdx@gmail.com>
2017-02-28 14:43:46 +0300

    Bad commit 2

--- CS/Example/MainWindow.xaml.cs
+++ CS/Example/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Example {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            int t=1234567sdfsdf;
+            int t=1234567sdfsdf2;
         }
     }
 }
