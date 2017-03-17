843f04d177a78b7b745d0efea1f64dde6b48f91f
DevExpressExampleBot <fakeEmail>
2017-03-17 08:41:27 +0300

    Update (generated VB)

--- VB/Example/MainWindow.xaml.vb
+++ VB/Example/MainWindow.xaml.vb
@@ -22,10 +22,12 @@ Namespace Example
 
 		Public Sub New()
 			InitializeComponent()
-
+		Dim testObject = DirectCast(New With, Object) {Key .pageIndex = 1, Key .isSuggestedSearch = 123}
+			Dim t As Integer=123456
+			t += 1
 		Dim btn As System.Windows.Controls.Button = New Button()
 			AddHandler btn.Click, Sub(sn, args)
-				Dim t2 As Integer = 123
+				Dim t2 As Integer = 1234
 				t2 += 1
 			End Sub
 		End Sub
